4a81477e990dd9e3dae2219aa1f50a1f200e5f6d
Ziga Babnik <zb1996@student.uni-lj.si>
2016-04-06 15:03:33 +0000

    Vmesni commit.

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -93,12 +93,18 @@ $(document).ready(function() {
       $('#poslji-sporocilo').focus();
     });
   });
-
+  
   socket.on('uporabniki', function(uporabniki) {
     $('#seznam-uporabnikov').empty();
+   
     for (var i=0; i < uporabniki.length; i++) {
-      $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
+        $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+     $('#seznam-uporabnikov div').click(function() {
+      $('#poslji-sporocilo').val("/zasebno " + $(this).text());
+      $('#poslji-sporocilo').focus();
+    });
   });
 
   setInterval(function() {
@@ -131,3 +137,5 @@ function dodajSmeske(vhodnoBesedilo) {
   }
   return vhodnoBesedilo;
 }
+
+
