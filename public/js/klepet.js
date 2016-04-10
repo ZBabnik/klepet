56524468081bcb714ecc44698f7de3dc30acca2b
Ziga Babnik <zb1996@student.uni-lj.si>
2016-04-10 11:10:04 +0000

    Zaključek dela na II. nalogi

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -36,6 +36,7 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
       besede.shift();
       var besedilo = besede.join(' ');
       var parametri = besedilo.split('\"');
+      parametri[3] = dodajSlike(parametri[3]);
       if (parametri) {
         this.socket.emit('sporocilo', { vzdevek: parametri[1], besedilo: parametri[3] });
         sporocilo = '(zasebno za ' + parametri[1] + '): ' + parametri[3];
@@ -50,3 +51,22 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
 
   return sporocilo;
 };
+
+
+function dodajSlike(vhodnoBesedilo) {
+  var tabBesedilo = vhodnoBesedilo.split(" ");
+  for(var i = 0; i < tabBesedilo.length; i++) {
+    if(tabBesedilo[i].indexOf('http://') == 0 || tabBesedilo[i].indexOf('https://') == 0) {
+      if(tabBesedilo[i].indexOf('.jpg') == (tabBesedilo[i].length - 4)) {
+        vhodnoBesedilo += "<div><img src="+tabBesedilo[i]+"></div>";
+      }
+      if(tabBesedilo[i].indexOf('.png') == (tabBesedilo[i].length - 4)) {
+        vhodnoBesedilo += "<div><img src="+tabBesedilo[i]+"></div>";
+      }
+      if(tabBesedilo[i].indexOf('.gif') == (tabBesedilo[i].length - 4)) {
+        vhodnoBesedilo += "<div><img src="+tabBesedilo[i]+"></div>";
+      }
+    } 
+  }
+  return vhodnoBesedilo;
+}
